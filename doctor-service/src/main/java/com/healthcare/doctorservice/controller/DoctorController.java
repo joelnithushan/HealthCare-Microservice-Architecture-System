e9e1a074e45e28845a57509705159bb129159981
joelnithushan <joelnithushan6@gmail.com>
2026-03-10 22:06:07 +0530

    feat: add doctor controller with crud endpoints

---
+++ doctor-service/src/main/java/com/healthcare/doctorservice/controller/DoctorController.java
@@ -0,0 +1,46 @@
+package com.healthcare.doctorservice.controller;
+
+import com.healthcare.doctorservice.dto.DoctorRequest;
+import com.healthcare.doctorservice.dto.DoctorResponse;
+import com.healthcare.doctorservice.service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/doctors")
+public class DoctorController {
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @PostMapping
+    public ResponseEntity<DoctorResponse> createDoctor(@RequestBody DoctorRequest request) {
+        DoctorResponse created = doctorService.createDoctor(request);
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DoctorResponse>> getAllDoctors() {
+        return ResponseEntity.ok(doctorService.getAllDoctors());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DoctorResponse> getDoctorById(@PathVariable Long id) {
+        return ResponseEntity.ok(doctorService.getDoctorById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DoctorResponse> updateDoctor(@PathVariable Long id, @RequestBody DoctorRequest request) {
+        return ResponseEntity.ok(doctorService.updateDoctor(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDoctor(@PathVariable Long id) {
+        doctorService.deleteDoctor(id);
+        return ResponseEntity.noContent().build();
+    }
+}
