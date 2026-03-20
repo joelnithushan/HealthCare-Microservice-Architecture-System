40a70192542813e511e00cb4e5554fb7fe482345
joelnithushan <joelnithushan6@gmail.com>
2026-03-20 12:45:07 +0530

    fix: remove team members' services from repo

--- appointment-service/src/main/java/com/healthcare/appointmentservice/repo/AppointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.healthcare.appointmentservice.repo;
-
-import com.healthcare.appointmentservice.model.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-}
--- notification-service/src/main/java/com/healthcare/notificationservice/model/NotificationStatus.java
+++
@@ -1,6 +0,0 @@
-package com.healthcare.notificationservice.model;
-
-public enum NotificationStatus {
-    READ,
-    UNREAD
-}
