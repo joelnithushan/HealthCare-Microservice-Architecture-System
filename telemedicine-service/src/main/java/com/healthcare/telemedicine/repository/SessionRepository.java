72e2202a195bc8562711ca960c807e9da0475c8d
joelnithushan <joelnithushan6@gmail.com>
2026-04-15 22:22:35 +0530

    add telemedicine service

---
+++ telemedicine-service/src/main/java/com/healthcare/telemedicine/repository/SessionRepository.java
@@ -0,0 +1,9 @@
+package com.healthcare.telemedicine.repository;
+
+import com.healthcare.telemedicine.model.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface SessionRepository extends JpaRepository<Session, Long> {
+    Optional<Session> findByAppointmentId(Long appointmentId);
+}
